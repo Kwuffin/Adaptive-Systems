75019993ca7b450f8355fbbbfe40bdc7a16f788d
Kwuffin <bestboykevin.bbg@gmail.com>
2021-11-22 15:26:56 +0100

    Added a Maze class

---
+++ Inleveropgave 1/3. Implementatie/Maze.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Maze:
+    def __init__(self):
+        loc = np.zeros((4, 4))
+        rew = np.zeros((4, 4))
+        act = [0, 1, 2, 3]
+
+    def step(self, current_state):
+        pass

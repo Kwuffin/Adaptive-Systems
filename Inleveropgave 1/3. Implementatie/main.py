00ce0ed033d66b86bb4cc23b1ee9fd0c590c2cf4
Kwuffin <bestboykevin.bbg@gmail.com>
2021-11-25 15:46:11 +0100

    Created instances of classes

--- Inleveropgave 1/3. Implementatie/main.py
+++ Inleveropgave 1/3. Implementatie/main.py
@@ -3,3 +3,7 @@ from Agent import Agent
 from Maze import Maze
 from Policy import Policy
 
+maze = Maze()
+policy = Policy()
+# agent = Agent(maze, policy, [np.random.randint(0, 3), np.random.randint(0, 3)])
+agent = Agent(maze, policy, [3, 2])

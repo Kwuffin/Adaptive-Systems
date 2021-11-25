a8a105d28ffd9303a7cd8d45fd5c23e42c2afe41
Kwuffin <bestboykevin.bbg@gmail.com>
2021-11-25 15:21:22 +0100

    Added documentation and did some cleanup

--- Inleveropgave 1/3. Implementatie/Maze.py
+++ Inleveropgave 1/3. Implementatie/Maze.py
@@ -7,19 +7,26 @@ class Maze:
                             [8, 9, 10, 11],
                             [4, 5, 6, 7],
                             [0, 1, 2, 3]])
-        self.rew = np.array([[-1, 1, -1, 0],
+        self.rew = np.array([[-1, 1, -1, 40],
+                            [-1, -1, -10, -10],
                             [-1, -1, -1, -1],
-                            [-1, -1, -1, -1],
-                            [0, -1, -1, -1]])
+                            [10, -2, -1, -1]])
         self.act = [0, 1, 2, 3]  # [up, right, down, left]
 
     def step(self, current_state, action):
+        """
+        Given a state and action, performs the action and returns the next state
+        :param current_state: State that agent is currently in
+        :param action: Action that is te be performed
+        :return: Next state after action is done.
+        """
+
         # current_state should look like this:
-        # [pos, reward: int/float, finished: bool]
+        # [pos: [int, int], reward: int/float, finished: bool]
         coord, rew, fin = current_state
         pos = self.loc[coord[0], coord[1]]
 
-        if action == 0:
+        if action == 0:  # Up
             if pos in self.loc[0]:
                 return current_state
             else:
@@ -27,7 +34,7 @@ class Maze:
                         self.rew[coord[0] - 1, coord[1]],
                         False]
 
-        elif action == 1:
+        elif action == 1:  # Right
             if pos in self.loc[:, 3]:
                 return current_state
             else:
@@ -35,19 +42,21 @@ class Maze:
                         self.rew[coord[0], coord[1] + 1],
                         False]
 
-        elif action == 2:
+        elif action == 2:  # Down
             if pos in self.loc[3]:
                 return current_state
             else:
                 return [[coord[0] + 1, coord[1]],
                         self.rew[coord[0] + 1, coord[1]],
                         False]
-        elif action == 3:
+
+        elif action == 3:  # Left
             if pos in self.loc[:, 0]:
                 return current_state
             else:
                 return [[coord[0], coord[1] - 1],
                         self.rew[coord[0], coord[1] - 1],
                         False]
+
         else:
             Exception(f"Action not possible, `action` must be an integer between 0-3, not {action}")

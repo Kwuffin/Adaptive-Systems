7fdb3cd9ece7353551729f277c4c93e84ae6092d
Kwuffin <bestboykevin.bbg@gmail.com>
2021-12-09 23:22:57 +0100

    Fixed comments

--- Inleveropgave 2/1. Model-Free prediction/main.py
+++ Inleveropgave 2/1. Model-Free prediction/main.py
@@ -9,32 +9,32 @@ agent_discounted = Agent(maze, 0.9)
 iterations = 100000
 
 # First visit Monte Carlo
-# print("First-Visit Monte Carlo:")
-# print("Random policy:")
-# agent.monte_carlo(iterations, random_policy)
-# print("-----------------")
-# agent_discounted.monte_carlo(iterations, random_policy)
-# print("===========================================\n")
-#
-# print("Optimal policy:")
-# agent.monte_carlo(iterations, optimal_policy)
-# print("-----------------")
-# agent_discounted.monte_carlo(iterations, optimal_policy)
-# print("===========================================\n")
-#
-# # Temporal Difference
-# print("Temporal Difference:")
-# print("Random policy:")
-# agent.temporal_difference(iterations, random_policy)
-# print("-----------------")
-# agent_discounted.temporal_difference(iterations, random_policy)
-# print("===========================================\n")
-#
-# print("Optimal policy:")
-# agent.temporal_difference(iterations, optimal_policy)
-# print("-----------------")
-# agent_discounted.temporal_difference(iterations, optimal_policy)
-# print("===========================================\n")
+print("First-Visit Monte Carlo:")
+print("Random policy:")
+agent.monte_carlo(iterations, random_policy)
+print("-----------------")
+agent_discounted.monte_carlo(iterations, random_policy)
+print("===========================================\n")
+
+print("Optimal policy:")
+agent.monte_carlo(iterations, optimal_policy)
+print("-----------------")
+agent_discounted.monte_carlo(iterations, optimal_policy)
+print("===========================================\n")
+
+# Temporal Difference
+print("Temporal Difference:")
+print("Random policy:")
+agent.temporal_difference(iterations, random_policy)
+print("-----------------")
+agent_discounted.temporal_difference(iterations, random_policy)
+print("===========================================\n")
+
+print("Optimal policy:")
+agent.temporal_difference(iterations, optimal_policy)
+print("-----------------")
+agent_discounted.temporal_difference(iterations, optimal_policy)
+print("===========================================\n")
 
 # On-policy first visit Monte Carlo
 print("On-policy First-Visit Monte Carlo:")
